aea0cf69c791216c55fddadd1fe56ba1f3f742a8
mcquaiga <mcquaiga@gmail.com>
2018-01-13 03:24:02 -0500

    todo web api

---
+++ Todo.ClientApp/ClientApp/app/components/todo/todo.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'todo',
+    templateUrl: './todo.component.html'
+})
+export class TodoComponent {
+    public forecasts: WeatherForecast[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
+            this.forecasts = result.json() as WeatherForecast[];
+        }, error => console.error(error));
+    }
+}
+
+interface WeatherForecast {
+    dateFormatted: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
+}
